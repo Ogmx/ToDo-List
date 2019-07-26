355450db7580a1cd6ae860db12808abbdcdc2f07
Ogmx <952345167@qq.com>
2019-07-26 13:36:56 +0800

    MVVM框架

---
+++ ToDo-List/ToDo-List/Model/UserMoudel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_List.Model
+{
+    class UserMoudel
+    {
+        public int ID {get; set;}
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+
+    }
+}
---
+++ ToDo-List/ToDo-List/ViewModel/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ToDo_List.ViewModel
+{
+    public class UserViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string Name)
+        {
+            if(Name != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(Name));
+            }
+        }
+    }
+}
